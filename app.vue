fdaddf0f3402df8437efc501d6bc8c44b5a648bc
Jhordeibys <jhordeibysdasilva@gmail.com>
2024-03-26 18:00:00 -0300

    templete project

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w3-content" style="max-width:1500px">
+   <Header/>
   </div>
+  <Footer/>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large icons"> 
+    <i class="fa-brands fa-tiktok"></i>
+    <i class="fa-brands fa-instagram w3-hover-opacity"></i>
+    <i class="fa-brands fa-facebook"></i>
+    <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+  </footer>
+</template>
+
+<style>
+.icons i {
+  margin-left: 10px;
+}
+</style>
